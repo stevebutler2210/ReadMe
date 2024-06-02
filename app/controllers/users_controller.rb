bb49f2ed7ca5b5803a5b3001aa1e60aa1faa0ce2
Steve Butler <stevebutler2210@users.noreply.github.com>
2024-06-02 15:55:37 +0100

    refactor: refactor user data response (#8)
    Simplifies the response from `Users#show` action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    render json: UserSerializer.new(user).serializable_hash.to_json
+    render json: UserSerializer.new(user).serializable_hash[:data][:attributes]
   end
 end
